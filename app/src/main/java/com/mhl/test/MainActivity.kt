ee5f7d2f52df7fcbb8f4ec78e4503c49a49da587
Михаил Байдуков <bajduckov2014@gmail.com>
2022-01-18 19:40:49 +0500

    added authentication

--- app/src/main/java/com/mhl/test/MainActivity.kt
+++ app/src/main/java/com/mhl/test/MainActivity.kt
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import java.util.regex.Pattern
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,24 +21,78 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth //Инициализирование аутентификации
+
         //Инициализация компонентов интерфейса
         emailText = findViewById(R.id.mail_input)
         passwordText = findViewById(R.id.password_input)
         loginButton = findViewById(R.id.login_button)
-        loginButton.setOnClickListener {
 
+        var emailString = emailText.text.toString()
+        var passwordString = passwordText.text.toString()
+
+        loginButton.setOnClickListener {
+            //Проверка Email и пароля на валидность
+            if (checkEmail(emailText.text.toString()) and  checkPassword(passwordText.text.toString())){
+                //Firebase вход через email и password, полученные из полей emailText и passwordText
+                signIn(emailText.text.toString(), passwordText.text.toString())
+            }
+            else if (!checkEmail(emailText.text.toString())){
+                emailText.error = "Вы ввели почту неверно"
+            }
+            else if (!checkPassword(passwordText.text.toString())){
+                passwordText.error = "Вы ввели пароль неверно"
+            }
         }
 
-        auth = Firebase.auth //Инициализирование аутентификации
+
 
     }
     override fun onStart() {
         super.onStart()
         val currentUser = auth.currentUser
         if (currentUser != null){
-            val intent : Intent = Intent(this, ObjectActivity::class.java)
-            startActivity(intent)
-            finish()
+            //Если пользователь авторизован, идёт переход на окно с объектами
+            intentToMain()
         }
+    }
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Если все данные введены верно и пользователь существует, выполняется переход на главное окно
+                    intentToMain()
+                } else {
+                    // Если вход провалился, то пользователь получит это сообщение в виде всплывающего окна
+                    Toast.makeText(this, "Такого пользователя не существует или вы ввели неправильный логин/пароль, попробуйте снова", Toast.LENGTH_SHORT).show()
+                }
+            }
+        // [END sign_in_with_email]
+    }
+
+
+    private fun intentToMain(){
+        val intent : Intent = Intent(this, ObjectActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+
+    private fun checkEmail(mail : String): Boolean {
+        var pattern = Pattern.compile("[a-zA-Z0-9\\_\\-]{3,32}" +
+                "\\@" +
+                "[a-zA-Z]{2,12}" +
+                "\\." +
+                "[a-zA-Z]{2,6}")
+        //Проверка email при помощи regex
+        return pattern.matcher(mail).matches()
+    }
+
+
+    private fun checkPassword(password : String) : Boolean {
+        var pattern = Pattern.compile("[a-zA-Z0-9]{6,30}")
+        return pattern.matcher(password).matches()
     }
 }
