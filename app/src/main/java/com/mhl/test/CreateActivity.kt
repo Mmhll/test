a4f38da63e0e1bf3d6471b5bb40b040872479c5c
Михаил Байдуков <bajduckov2014@gmail.com>
2022-01-18 23:38:12 +0500

    added init in CreateActivity.kt

--- app/src/main/java/com/mhl/test/CreateActivity.kt
+++ app/src/main/java/com/mhl/test/CreateActivity.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.MediaStore.Images.Media.getBitmap
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import com.google.firebase.storage.FirebaseStorage
@@ -26,25 +27,38 @@ import com.google.firebase.storage.ktx.storageMetadata
 import com.mhl.test.databinding.ActivityCreateBinding
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.floor
 
 class CreateActivity : AppCompatActivity() {
+    //Объявление переменных
     private lateinit var addImageButton: Button
-    private lateinit var imageView: ImageView
-    val GALLERY_REQUEST = 1
-    val formatter = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss")
+    private lateinit var addressText :EditText
+    private lateinit var spaceText: EditText
+    private lateinit var costText: EditText
+    private lateinit var roomText: EditText
+    private lateinit var floorText: EditText
     private lateinit var selectedImage : Uri
     lateinit var now : Date
+    //Инициализация констант
+    val GALLERY_REQUEST = 1
+    val formatter = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
-
+        //Инициализация элементов интерфейса
+        addressText = findViewById(R.id.address_input)
+        spaceText = findViewById(R.id.space_input)
+        costText = findViewById(R.id.cost_input)
+        roomText = findViewById(R.id.rooms_input)
+        floorText = findViewById(R.id.floor_input)
         addImageButton = findViewById(R.id.add_image_button)
 
-        addImageButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK)
-            intent.type = "image/*"
-            startActivityForResult(intent, GALLERY_REQUEST)
+        addImageButton.setOnClickListener {//Добавление слушателя для выбора изображения из галереи
+            val intent = Intent(Intent.ACTION_PICK) //Параметр для intent, который даёт возможность выбрать какой-либо файл
+            intent.type = "image/*"//Задание типа изображения для intent
+            startActivityForResult(intent, GALLERY_REQUEST)//Использование intent для получения результата (изображения)
         }
     }
 
@@ -54,13 +68,9 @@ class CreateActivity : AppCompatActivity() {
         when(requestCode){
             GALLERY_REQUEST ->
                 if (resultCode == RESULT_OK){
-                    selectedImage = imageReturnedIntent?.data!!
-                    now = Date()
-                    val fileName = formatter.format(now)
+                    selectedImage = imageReturnedIntent?.data!!//Инициализация Uri переменной, которая несёт в себе ссылку на файл
                     Toast.makeText(this, "Файл выбран", Toast.LENGTH_SHORT).show()
-                    addImageButton.setBackgroundColor(resources.getColor(R.color.green))
-                    var firebaseStorage = FirebaseStorage.getInstance().getReference("images/$fileName")
-                    firebaseStorage.putFile(selectedImage)
+                    addImageButton.setBackgroundColor(resources.getColor(R.color.green))//Смена цвета заднего фона кнопки для того, чтобы лучше было понятно, сделано ли это действие
                 }
         }
 
